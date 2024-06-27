91e5fa9571d3093a28cb5a6c4b3f41fb4537fc4b
Trần Minh Thông <98743684+TMThong@users.noreply.github.com>
2024-06-27 14:37:04 +0700

    Update Animation.cs

--- Data/User/Animation.cs
+++ Data/User/Animation.cs
@@ -6,5 +6,8 @@ using System.Threading.Tasks;
 
 namespace Gopet.Data.User
 {
-    public record Animation(sbyte numFrame, string frameImgPath, int vX, int vY, bool isDrawEnd, bool mirrorWithChar, sbyte type);
+    public record Animation(sbyte numFrame, string frameImgPath, int vX, int vY, bool isDrawEnd, bool mirrorWithChar, sbyte type)
+    {
+        public const sbyte TYPE_ARCHIVENMENT = 0;
+    }
 }
