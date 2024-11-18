69d34903c4216f595a2f77057ae6d6726b23367d
Trần Minh Thông <mthongdev@gmail.com>
2024-11-18 10:16:21 +0700

    test expression and test ping

---
+++ PingServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+
+namespace PingServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = "160.30.160.83";
+            int port = 19189;
+            TcpClient tcpClient = new TcpClient();
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            tcpClient.Connect(ip, port);
+            stopwatch.Stop();
+            tcpClient.Close();
+            Console.WriteLine($"KET NOI MAT TG: {stopwatch.Elapsed.TotalMilliseconds}");
+            Console.ReadKey();
+        }
+    }
+}
