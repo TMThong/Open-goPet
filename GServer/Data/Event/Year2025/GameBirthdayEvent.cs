59a7464f25e99ac7f5d0ed349bfc4bd5f500906e
Trần Minh Thông <mthongdev@gmail.com>
2025-01-16 12:00:29 +0700

    Create GameBirthdayEvent.cs

---
+++ GServer/Data/Event/Year2025/GameBirthdayEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gopet.Data.Event.Year2025
+{
+    public class GameBirthdayEvent : EventBase
+    {
+        public static readonly GameBirthdayEvent Instance = new GameBirthdayEvent();
+        protected GameBirthdayEvent()
+        {
+            this.Name = "Sự kiện sinh nhật";
+        }
+
+        public override bool Condition => true;
+
+        public override void UseItem(int itemId, Player player)
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public override void NpcOption(Player player, int optionId)
+        {
+
+        }
+    }
+}
