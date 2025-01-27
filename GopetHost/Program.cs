5fce8c2aae9359f08e9a8176f3cfacf690f966bb
Trần Minh Thông <mthongdev@gmail.com>
2025-01-27 21:07:50 +0700

    add icons

--- GopetHost/Program.cs
+++ GopetHost/Program.cs
@@ -33,18 +33,15 @@ namespace GopetHost
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession(options =>
             {
-                options.IdleTimeout = TimeSpan.FromMinutes(15);
+                options.IdleTimeout = TimeSpan.FromHours(2);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.Use(async (ctx, next) =>
