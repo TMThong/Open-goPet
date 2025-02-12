13e0d522581b178597bc9330325372d663230f74
Trần Minh Thông <mthongdev@gmail.com>
2025-02-12 20:35:38 +0700

    Create LoginHistory.cs

---
+++ GServer/Data/User/LoginHistory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gopet.Data.User
+{
+    /// <summary>
+    /// Lịch sử đăng nhập
+    /// </summary>
+    public class LoginHistory
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Tên đăng nhập
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Mật khẩu đã thử
+        /// </summary>
+        public string TryPassword { get; set; }
+        /// <summary>
+        /// Thời gian đăng nhập
+        /// </summary>
+        public DateTime LoginTime { get; set; }
+        /// <summary>
+        /// Địa chỉ IP
+        /// </summary>
+        public string IPAddress { get; set; }
+        /// <summary>
+        /// Kết quả đăng nhập
+        /// </summary>
+        public bool IsSuccess { get; set; } = false;
+        /// <summary>
+        /// Đăng nhập qua web
+        /// </summary>
+        public bool IsWebLogin { get; set; } = false;
+    }
+}
